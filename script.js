f183720a146c26c04336173c5422500f0c5e04de
Vinícius Simioni <viniciussimioni@outlook.com>
2022-07-22 15:00:22 -0300

    draw grid function working

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-//adiciona divs no container principal
-for(var i = 0; i< 25; i++){
-    document.getElementById("container").innerHTML += "<div class='row'></div>";
-    for(var j=0; j<25; j++){
-        document.getElementsByClassName("row")[i].innerHTML += "<div class='column'></div>";
+//add div when opening page
+function drawGrid(gridsize){
+    for(var i = 0; i<gridsize; i++){
+        document.getElementById("container").innerHTML += "<div class='row'></div>";
+        for(var j=0; j<gridsize; j++){
+            document.getElementsByClassName("row")[i].innerHTML += "<div class='column'></div>";
+        }
     }
 }
+//call starting grid
+drawGrid(20);
+
+//change grid layout
+function getInput(){
+    //read input value
+    var gridsize = document.getElementById("inputbox").value;
+    //remove latest container
+    document.getElementById("container").remove();
+    //add a new container
+    document.getElementById("body").innerHTML += "<div id='container'></div>";
+    //draw the grid
+    console.log(gridsize);
+    for(var i = 0; i<gridsize; i++){
+        document.getElementById("container").innerHTML += "<div class='row'></div>";
+        for(var j=0; j<gridsize; j++){
+            document.getElementsByClassName("row")[i].innerHTML += "<div class='column'></div>";
+        }
+    }
+}
+
